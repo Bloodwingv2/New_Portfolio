5a40b3573e7344ecf9b91f40b221540becac336e
Bloodwingv2 <bhandariumesh81@gmail.com>
2026-03-01 18:11:56 +0100

    - react fix

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -3,7 +3,7 @@ import Layout from './components/Layout';
 import ChatInterface from './components/ChatInterface';
 import SplashScreen from './components/SplashScreen';
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   const [loading, setLoading] = useState(true);
